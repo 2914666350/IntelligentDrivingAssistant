e03a1860103bca45c61e1ec209f624673de09cfa
刘书振 <Liu2914666350@163.com>
2021-03-07 16:07:42 +0800

    修改LoginActivity

--- app/src/main/java/com/example/intelligentdrivingassistant/LoginActivity.java
+++ app/src/main/java/com/example/intelligentdrivingassistant/LoginActivity.java
@@ -2,152 +2,151 @@ package com.example.intelligentdrivingassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Handler;
 
+import android.preference.PreferenceActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.mapapi.http.AsyncHttpClient;
+
+import java.net.URLEncoder;
+
 public class LoginActivity extends AppCompatActivity {
-    private EditText et_username, et_password;
-    private Button btn_login;
-    private TextView tv_forgot_pwd,tv_newUser;
+    private EditText name;//用户名
+    private EditText password;//用户密码
+    private Button login;//登录按钮
+    private TextView register;//注册
+    private TextView forgetNum;//忘记密码
+    private myDatabaseHelper dbHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
-        et_username = (EditText) findViewById(R.id.et_username);
-        et_password = (EditText) findViewById(R.id.et_password);
-        btn_login = (Button) findViewById(R.id.btn_login);
-        tv_forgot_pwd = (TextView) findViewById(R.id.tv_forgot_pwd);
-        tv_newUser = (TextView) findViewById(R.id.tv_newUser);
-        LoginOnClickListener loginOnClickListener = new LoginOnClickListener();
-        btn_login.setOnClickListener(loginOnClickListener);
-    }
 
-    private class LoginOnClickListener implements View.OnClickListener {
-        Handler handler=new Handler(){
-            public void handleMessage(android.os.Message msg) {
-                switch (msg.what) {
-                    case 1:
-                        Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_LONG).show();
+        dbHelper = myDatabaseHelper.getInstance(this);
 
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        break;
 
-                    default:
-                        break;
+        name = (EditText) findViewById(R.id.admin_login_activity_name_input);
+        password = (EditText) findViewById(R.id.admin_login_activity_password_input);
+        login = (Button) findViewById(R.id.admin_login_activity_login);
+        register = (TextView) findViewById(R.id.admin_login_activity_register);
+        forgetNum = (TextView) findViewById(R.id.admin_login_activity_forgetNum);
+
+        //跳转到登录过的管理员界面
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nameInfo = name.getText().toString();
+                String passwordInfo = password.getText().toString();
+                //从数据库中获取密码并判断是否相同
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Cursor cursor = db.rawQuery("select password from admin where name=?", new String[]{nameInfo});
+                String pi = null;
+                if (cursor.moveToNext()) {
+                    pi = cursor.getString(cursor.getColumnIndex("password"));//获取密码
                 }
-
-            };
-        };
-        @Override
-        public void onClick(View v) {
-            //控件被点击后执行的代码
-//            if(v.getId() == R.id.btn_login) { //返回上一级的按钮被单击
-//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                startActivity(intent);
-////                overridePendingTransition(R.anim.down_out, R.anim.down_in);
-//            }
-             if(v.getId() == R.id.tv_newUser) { //用户注册的按钮或新用户的文本域被单击
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-//                overridePendingTransition(R.anim.go_in, R.anim.go_out);
-            }
-            else if(v.getId() == R.id.tv_forgot_pwd) { //忘记密码的文本域被单击
-                Intent intent = new Intent(LoginActivity.this, ForgotPwdActivity.class);
-                startActivity(intent);
-//                overridePendingTransition(R.anim.go_in, R.anim.go_out);
-            }
-            else if(v.getId() == R.id.btn_login) { //用户登录的按钮被单击
-                final String userName = et_username.getText().toString().trim();
-                final String password = et_password.getText().toString().trim();
-                if (userName.equals("") || password.equals("")) { //用户名或者密码未填写
-                    Toast.makeText(getApplicationContext(), "请将用户名密码填写完全后再登录", Toast.LENGTH_LONG).show();
+                //密码正确跳转到登录后的界面
+                if (passwordInfo.equals(pi)) {
+                    Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    cursor.close();
                 } else {
-                    //用户名密码均已正确填写
-                    //模拟登录过程
-                    final ProgressDialog pd = new ProgressDialog(LoginActivity.this);
-                    pd.setMessage("正在登录...");
-                    pd.show();
-                    new Thread() {
-                        public void run() {
-                            try {
-                                Thread.sleep(3000);
-                                handler.sendEmptyMessage(1);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            pd.dismiss();
-                        } ;
-                    }.start();
-                /*new Thread() {
-						public void run() {
-							final String result = LoginService.loginByClientPost(userName, password);
-							if(result != null) {
-								runOnUiThread(new Runnable() {
-									@Override
-									public void run() {
-	Toast.makeText(LoginActivity.this, result, Toast.LENGTH_LONG).show();
-	}
-	});
-	Intent intent = new Intent(LoginActivity.this, MainTabActivity.class);
-	startActivity(intent);
-							}
-	else{//请求失败
-	runOnUiThread(new Runnable() {
-									@Override
-									public void run() {
-Toast.makeText(LoginActivity.this, "请求超时...", Toast.LENGTH_LONG).show();
-									}
-	});
-	}
-	};
-	}.start();*/
-//通过android-async-http-1.4.6框架完成与服务器端的交互
-                    //1.GET提交方式
-					/*AsyncHttpClient client = new AsyncHttpClient();
-					String path = "http://172.21.106.59:8080/CoffeeOnlineServer/servlet/LoginServlet?userName="
-							+ URLEncoder.encode(userName) + "&password=" + URLEncoder.encode(password);
-					client.get(path, new AsyncHttpResponseHandler() {
-
-						@Override
-						public void onSuccess(int arg0, Header[] arg1, byte[] response) {
-							Toast.makeText(LoginActivity.this, "请求成功:"+new String(response), Toast.LENGTH_LONG).show();
-						}
-
-						@Override
-						public void onFailure(int arg0, Header[] arg1, byte[] response, Throwable arg3) {
-							Toast.makeText(LoginActivity.this, "请求失败:"+new String(response), Toast.LENGTH_LONG).show();
-						}
-					});*/
-                    //2.POST提交方式
-					/*AsyncHttpClient client = new AsyncHttpClient();
-					String url = IPProvider.IP_ADDRESS+"CoffeeOnlineServer/servlet/LoginServlet";
-					RequestParams params = new RequestParams();
-					params.put("userName", userName);
-					params.put("password", password);
-					client.post(url, params, new AsyncHttpResponseHandler() {
-						@Override
-						public void onSuccess(int arg0, Header[] arg1, byte[] response) {
-							Toast.makeText(LoginActivity.this, "请求成功:"+new String(response), Toast.LENGTH_LONG).show();
-						}
-						@Override
-						public void onFailure(int arg0, Header[] arg1, byte[] arg2, Throwable arg3) {
-							Toast.makeText(LoginActivity.this, "请求失败:"+arg3.getMessage(), Toast.LENGTH_LONG).show();
-						}
-					});*/
+                    Toast.makeText(LoginActivity.this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
                 }
+
+
             }
-        }
+        });
+
+        //自定义AlertDialog用于注册
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                LayoutInflater factory = LayoutInflater.from(LoginActivity.this);
+                final View textEntryView = factory.inflate(R.layout.activity_register, null);
+                builder.setTitle("用户注册");
+                builder.setView(textEntryView);
+
+                final EditText code = (EditText) textEntryView.findViewById(R.id.admin_register_info);
+                final EditText name = (EditText) textEntryView.findViewById(R.id.admin_register_name);
+                final EditText firstPassword = (EditText) textEntryView.findViewById(R.id.admin_register_first_password);
+                final EditText secondPassword = (EditText) textEntryView.findViewById(R.id.admin_register_second_password);
+
+
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String codeInfo = code.getText().toString();
+                        //注册码要为10086
+                        if (codeInfo.equals("10086")) {
+                            String nameInfo = name.getText().toString();
+                            String firstPasswordInfo = firstPassword.getText().toString();
+                            String secondPasswordInfo = secondPassword.getText().toString();
+                            SQLiteDatabase db = dbHelper.getWritableDatabase();
+                            //检测密码是否为6个数字
+                            if (firstPasswordInfo.matches("[0-9]{6}")) {
+                                // 两次密码是否相同
+                                if (firstPasswordInfo.equals(secondPasswordInfo)) {
+                                    Cursor cursor = db.rawQuery("select name from admin where name=? ", new String[]{nameInfo});
+                                    //用户是否存在
+                                    if (cursor.moveToNext()) {
+                                        Toast.makeText(LoginActivity.this, "该用户已存在", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        db.execSQL("insert into admin(name,password)values(?,?)", new String[]{nameInfo, firstPasswordInfo});
+                                    }
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "两次密码不相同", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(LoginActivity.this, "密码为6位纯数字", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, "注册码错误", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+                builder.create().show();
+
+            }
+        });
+
+        forgetNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "此功能暂不支持", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
 
